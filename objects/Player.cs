4a78590a257f07a21db5bf37dec18b3b1cdf57a2
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-01-09 23:07:01 -0700

    changes to project structure, added couple classes

---
+++ objects/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace TheFountainOfObjects;
+
+public class Player : GameObject
+{
+    
+
+    public override void Move(Direction direction)
+    { 
+        Position = direction switch
+        {
+            Direction.East => (Position.row, Position.column + 1),
+            Direction.West => (Position.row, Position.column - 1),
+            Direction.North => (Position.row - 1, Position.column),
+            Direction.South => (Position.row + 1, Position.column),
+            _ => Position
+        };
+    }
+    
+    public override void GetPosition()
+    {
+        Console.WriteLine($"You are in the room at row: {Position.row + 1}, column: {Position.column + 1}");
+    }
+}
---
+++ objects/rooms/Room.cs
@@ -0,0 +1,34 @@
+﻿namespace TheFountainOfObjects;
+
+public abstract class Room(int row, int column, RoomType roomType)
+{
+    public (int row, int column) RoomLocation { get; init; } = (row, column);
+    private List<GameObject> insideRoom = new ();
+    internal RoomType RoomType { get; } = roomType;
+    private bool _isEmpty = true;
+
+    public abstract RoomType IdentifyRoom();
+    
+    public bool IsEmpty()
+    {
+        return _isEmpty;
+    }
+    
+    public void SetEmpty()
+    {
+        if (insideRoom.Count == 0)
+        {
+            _isEmpty = true;
+        }
+        else
+        {
+            Console.WriteLine("This room is not empty.");
+        }
+    }
+    
+    public void SetOccupancy(GameObject gameObject)
+    {
+        _isEmpty = false;
+        insideRoom.Add(gameObject);
+    }
+}
