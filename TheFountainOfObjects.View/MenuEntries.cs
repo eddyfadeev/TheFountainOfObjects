3c1ed52e940dbf80a25911f0b9209b22b4391bd4
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-05-01 23:33:48 -0600

    Small adjustments, added menu

---
+++ TheFountainOfObjects.View/MenuEntries.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TheFountainOfObjects.Model.Services.CustomEnumAttributes;
+
+namespace TheFountainOfObjects.View;
+
+public enum MenuEntries
+{
+    [Display(Name = "Start Game"), Method("StartGame")]
+    StartGame,
+    [Display(Name = "Settings"), Method("ShowSettings")]
+    Settings,
+    [Display(Name = "Show Help"), Method("ShowHelp")]
+    ShowHelp,
+    [Display(Name = "Exit")]
+    Exit
+}
